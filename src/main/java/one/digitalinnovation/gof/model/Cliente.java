1db633fdf6d0296fcfe3047631472f7025047400
Redesu <willyanmaster@gmail.com>
2023-10-21 16:16:15 -0300

    Initial commit

--- src/main/java/one/digitalinnovation/gof/model/Cliente.java
+++ src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -5,39 +5,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import lombok.Data;
 
 @Entity
+@Data
 public class Cliente {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	private String nome;
-	@ManyToOne
-	private Endereco endereco;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public Endereco getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
-	}
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    private Endereco endereco;
 }
